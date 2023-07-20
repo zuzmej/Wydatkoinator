e5aee8e4c7c402d6fb718424a102ac1447cfcde5
PartyKusZ <jakub.kusz@onet.pl>
2023-07-20 11:34:03 +0200

    1. modyfikacja api Database 2. Napisanie drivera pokazującego jak kożystać z Database

--- src/database/database.py
+++ src/database/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, joinedload
 from base import Base
 from category import Category
 from expense import Expense
+from datetime import datetime
 
 class Database:
     def __init__(self, db_string: str):
@@ -29,55 +30,71 @@ class Database:
         if not category:
             raise ValueError(f"No category found with id {category_id}")
         
+        date = datetime.strptime(date, "%Y-%m-%d").date()
         session.add(Expense(amount=amount, date=date, category_id=category_id))
         session.commit()
         session.close()
     
-    def get_categories(self):
+    def get_all_categories(self):
         session = self.Session()
         categories = session.query(Category).all()
         session.close()
         return categories
     
-    def get_category(self, category_id: int):
+    def get_category_by_id(self, category_id: int):
         session = self.Session()
         category = session.query(Category).filter_by(id=category_id).first()
         session.close()
         return category
     
-    def get_expenses(self):
-        session = self.Session()
-        expenses = session.query(Expense).all()
-        session.close()
-        return expenses
-    
-    def get_expenses_with_category_name(self):
+    def get_all_expenses(self):
         session = self.Session()
         expenses = session.query(Expense).options(joinedload(Expense.category)).all()
         session.close()
         return expenses
     
-    def get_expenses_by_id(self, expense_id: int):
+    def get_expense_by_id(self, expense_id: int):
         session = self.Session()
-        expenses = session.query(Expense).filter_by(id=expense_id).first()
+        expenses = session.query(Expense).options(joinedload(Expense.category)).filter_by(id=expense_id).first()
         session.close()
         return expenses
     
     def get_expenses_by_date(self, date: str):
         session = self.Session()
+        date = datetime.strptime(date, "%Y-%m-%d").date()
         expenses = session.query(Expense).options(joinedload(Expense.category)).filter_by(date=date).all()
         session.close()
         return expenses
     
-    def get_expenses_by_category_id(self, category_id: int):
+    def get_expenses_by_date_range(self, start_date: str, end_date: str):
         session = self.Session()
-        expenses = session.query(Expense).options(joinedload(Expense.category)).filter_by(category_id=category_id).all()
+        start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+        expenses = session.query(Expense).options(joinedload(Expense.category)).filter(Expense.date.between(start_date, end_date)).all()
+        session.close()
+        return expenses
+    
+    def get_expenses_by_category_id_in_date_range(self, category_id: int, start_date: str, end_date: str):
+        session = self.Session()
+        start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        expenses = session.query(Expense).options(joinedload(Expense.category)).filter(Expense.category_id == category_id,Expense.date.between(start_date, end_date)).all()
         session.close()
         return expenses
     
     
-    def get_expenses_by_category_name(self, category_name: str):
+    def get_expenses_by_category_name_in_date_range(self, category_name: str, start_date: str, end_date: str):
         session = self.Session()
-        expenses = session.query(Expense).join(Category).filter(Category.name == category_name).options(joinedload(Expense.category)).all()
+        start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+        expenses = session.query(Expense).options(joinedload(Expense.category)).join(Category).filter(Category.name == category_name, Expense.date.between(start_date, end_date)).all()
+        session.close()
+        return expenses
+    
+    def get_expenses_in_date_and_cost_range(self, start_date: str, end_date: str, min_cost: float, max_cost: float):
+        session = self.Session()
+        start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+        expenses = session.query(Expense).options(joinedload(Expense.category)).filter(Expense.date.between(start_date, end_date),Expense.amount.between(min_cost, max_cost)).all()
         session.close()
         return expenses
--- src/database/driver.py
+++ src/database/driver.py
@@ -1,76 +1,69 @@
 from database import Database
-from datetime import date
+db = Database("expenses.db")
 
-from database import Database
-from datetime import date
+# Add categories
+try:
+    print("\nAdding categories...")
+    db.add_category("Groceries")
+    db.add_category("Rent")
+    db.add_category("Transport")
+except ValueError as e:
+    print(e)  # handle error if category already exists
 
-# Utworzenie bazy danych
-db = Database('expenses.db')
+# Add expenses
+print("\nAdding expenses...")
+db.add_expense(amount=100.50, date="2023-07-01", category_id=1)
+db.add_expense(amount=800.00, date="2023-07-05", category_id=2)
+db.add_expense(amount=50.00, date="2023-07-10", category_id=3)
 
-# Dodawanie kategorii
-categories = ['Food', 'Rent', 'Entertainment', 'Transport', 'Utilities']
+# Get all categories
+print("\nGetting all categories...")
+categories = db.get_all_categories()
 for category in categories:
-    try:
-        db.add_category(category)
-    except ValueError as e:
-        print(e)
+    print(category.name)
 
-# Dodawanie wydatków
-expenses = [
-    {'amount': 150.0, 'date': date.today(), 'category_id': 1},
-    {'amount': 1000.0, 'date': date.today(), 'category_id': 2},
-    {'amount': 75.0, 'date': date.today(), 'category_id': 3},
-    {'amount': 50.0, 'date': date.today(), 'category_id': 4},
-    {'amount': 200.0, 'date': date.today(), 'category_id': 5}
-]
+# Get category by id
+print("\nGetting category by id...")
+category = db.get_category_by_id(1)
+print(f"Category ID: {category.id}, Name: {category.name}")
+
+# Get all expenses
+print("\nGetting all expenses...")
+expenses = db.get_all_expenses()
 for expense in expenses:
-    try:
-        db.add_expense(**expense)
-    except ValueError as e:
-        print(e)
+    print(f"Expense ID: {expense.id}, Amount: {expense.amount}, Date: {expense.date}, Category: {expense.category.name}")
 
-# Wyświetlanie wszystkich kategorii
-print("\nAll Categories:")
-all_categories = db.get_categories()
-for category in all_categories:
-    print(f"ID: {category.id}, Name: {category.name}")
+# Get expense by id
+print("\nGetting expense by id...")
+expense = db.get_expense_by_id(1)
+print(f"Expense ID: {expense.id}, Amount: {expense.amount}, Date: {expense.date}, Category: {expense.category.name}")
 
-# Wyświetlanie kategorii po ID
-print("\nCategory with ID=3:")
-category = db.get_category(3)
-print(f"ID: {category.id}, Name: {category.name}")
+# Get expenses by date
+print("\nGetting expenses by date...")
+expenses_by_date = db.get_expenses_by_date("2023-07-01")
+for expense in expenses_by_date:
+    print(f"Expense ID: {expense.id}, Amount: {expense.amount}, Date: {expense.date}, Category: {expense.category.name}")
 
-# Wyświetlanie wszystkich wydatków
-print("\nAll Expenses:")
-all_expenses = db.get_expenses()
-for expense in all_expenses:
-    print(f"ID: {expense.id}, Date: {expense.date}, Amount: {expense.amount}, Category ID: {expense.category_id}")
+# Get expenses by date range
+print("\nGetting expenses by date range...")
+expenses_by_date_range = db.get_expenses_by_date_range("2023-07-01", "2023-07-10")
+for expense in expenses_by_date_range:
+    print(f"Expense ID: {expense.id}, Amount: {expense.amount}, Date: {expense.date}, Category: {expense.category.name}")
 
-# Wyświetlanie wszystkich wydatków wraz z nazwą kategorii
-print("\nAll Expenses with Category Name:")
-expenses_with_category_name = db.get_expenses_with_category_name()
-for expense in expenses_with_category_name:
-    print(f"ID: {expense.id}, Date: {expense.date}, Amount: {expense.amount}, Category: {expense.category.name}")
+# Get expenses by category id in date range
+print("\nGetting expenses by category id in date range...")
+expenses_by_category_id_date_range = db.get_expenses_by_category_id_in_date_range(1, "2023-07-01", "2023-07-10")
+for expense in expenses_by_category_id_date_range:
+    print(f"Expense ID: {expense.id}, Amount: {expense.amount}, Date: {expense.date}, Category: {expense.category.name}")
 
-# Wyświetlanie wydatku po ID
-print("\nExpense with ID=2:")
-expense = db.get_expenses_by_id(2)
-print(f"ID: {expense.id}, Date: {expense.date}, Amount: {expense.amount}, Category ID: {expense.category_id}")
-
-# Wyświetlanie wydatków na podstawie daty
-print("\nExpenses on Date:")
-date_expenses = db.get_expenses_by_date(date.today())
-for expense in date_expenses:
-    print(f"ID: {expense.id}, Date: {expense.date}, Amount: {expense.amount}, Category: {expense.category.name}")
-
-# Wyświetlanie wydatków na podstawie kategorii
-print("\nExpenses by Category ID:")
-expenses_by_category_id = db.get_expenses_by_category_id(2)
-for expense in expenses_by_category_id:
-    print(f"ID: {expense.id}, Date: {expense.date}, Amount: {expense.amount}, Category: {expense.category.name}")
-
-# Wyświetlanie wydatków na podstawie nazwy kategorii
-print("\nExpenses by Category Name:")
-expenses_by_category_name = db.get_expenses_by_category_name('Rent')
+# Get expenses by category name
+print("\nGetting expenses by category name...")
+expenses_by_category_name = db.get_expenses_by_category_name_in_date_range("Rent", "2023-07-01", "2023-07-10")
 for expense in expenses_by_category_name:
-    print(f"ID: {expense.id}, Date: {expense.date}, Amount: {expense.amount}, Category: {expense.category.name}")
+    print(f"Expense ID: {expense.id}, Amount: {expense.amount}, Date: {expense.date}, Category: {expense.category.name}")
+
+print("Getting expenses in date and cost range...")
+expenses_in_range = db.get_expenses_in_date_and_cost_range("2023-07-01", "2023-07-31", 50.00, 200.00)
+for expense in expenses_in_range:
+    print(f"Expense ID: {expense.id}, Amount: {expense.amount}, Date: {expense.date}, Category: {expense.category.name}")
+
