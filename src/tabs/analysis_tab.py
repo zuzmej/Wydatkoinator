235b967a1b9d17ffd13495cda9dcdb9d17f5478d
Jakub Kusz <jakub.kusz@onet.pl>
2023-08-18 21:08:28 +0200

    dodanie obsługi wyboru daty

--- src/tabs/analysis_tab.py
+++ src/tabs/analysis_tab.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QWidget
+from datetime import datetime, timedelta
 from src.ui.analysis_tab import Ui_analisys_tab
 
 class Analysis_tab(QWidget, Ui_analisys_tab):
@@ -7,8 +8,13 @@ class Analysis_tab(QWidget, Ui_analisys_tab):
         self.setupUi(self)
         self.is_pie_chart_set = False
         self.is_stack_chart_set = False
+        self.date_from.setDate(datetime.today().date() - timedelta(days=7))
+        self.date_to.setDate(datetime.today().date())
+        self.date_from.setCalendarPopup(True)
+        self.date_to.setCalendarPopup(True)
         self.pie_chart_button.clicked.connect(self.set_pie_chart)
         self.stack_chart_button.clicked.connect(self.set_stack_chart)
+        
 
     def set_pie_chart(self):
         self.is_pie_chart_set = True
