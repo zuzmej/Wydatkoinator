470532e1d3b6555fbd232135abbdc9bab1b5b879
PartyKusZ <jakub.kusz@onet.pl>
2023-09-02 19:22:29 +0200

    fajnie działa

---
+++ tests/history_tab_tests.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.path.insert(0, '../')
+from src.tabs.history_tab import History_tab
+from PyQt5 import QtWidgets
+from src.database.database import Database
+db = Database("expenses.db")
+app = QtWidgets.QApplication(sys.argv)
+
+w = History_tab()
+w.set_database(db)
+w.set_default_history_list()
+w.show()
+
+sys.exit(app.exec_())
